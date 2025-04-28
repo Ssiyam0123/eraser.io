0bd6249dbea4e233a436b8f80e102408927cf3df
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-29 01:49:23 +0600

    users data are inserting in db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,41 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex, useMutation, useQuery } from "convex/react";
+import React, { useEffect } from "react";
 
 export default function Dashboard() {
+  const { user }: any = useKindeBrowserClient();
+  const convex = useConvex();
+  const getUser = useQuery(api.user.getUser, { email: user?.email });
+
+  const createUser = useMutation(api.user.createUser);
+
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, { email:user?.email });
+    if (!result?.length) {
+      createUser({
+        name: user.given_name,
+        email: user.email,
+        image: user?.picture,
+      }).then((resp) => console.log(resp));
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      checkUser();
+    }
+  }, [user]);
   return (
     <div>
-      <Button><LogoutLink>Logout</LogoutLink></Button>
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
     </div>
-  )
+  );
 }
